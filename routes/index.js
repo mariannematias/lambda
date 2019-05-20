090420cbce43ac72c31653254896b59fcd9215c3
mariannematias <mariannec.matias@gmail.com>
2019-05-20 10:53:16 +0800

    add homepage

---
+++ routes/index.js
@@ -0,0 +1,83 @@
+// Full Documentation - https://docs.turbo360.co/
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+const controllers = require('../controllers')
+
+const CDN = (process.env.TURBO_ENV=='dev') ? null : process.env.CDN
+
+router.get('/', (req, res) => {
+	const data = {cdn: CDN}
+	turbo.pageConfig('home', process.env.TURBO_API_KEY, process.env.TURBO_ENV)
+	.then(homeConfig => {
+		data['page'] = homeConfig
+		let ctr = new controllers.post()
+		return ctr.get()
+	})
+	.then(posts => {
+		data['posts'] = posts
+		return turbo.currentApp(process.env.TURBO_ENV)
+	})
+	.then(site => {
+		data['site'] = site
+		data['global'] = site.globalConfig
+		res.render('home', data)
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+router.get('/blog', (req, res) => {
+	const data = {cdn: CDN}
+
+	let ctr = new controllers.post()
+	ctr.get()
+	.then(posts => {
+		data['posts'] = posts
+		return turbo.currentApp(process.env.TURBO_ENV)
+	})
+	.then(site => {
+		data['site'] = site
+		data['global'] = site.globalConfig
+		res.render('blog', data)
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+router.get('/post/:slug', (req, res) => {
+	const data = {cdn: CDN}
+
+	let ctr = new controllers.post()
+	ctr.get({slug:req.params.slug})
+	.then(posts => {
+		if (posts.length == 0){
+			throw new Error('Post '+req.params.slug+' not found.')
+			return
+		}
+
+		data['post'] = posts[0]
+		return turbo.currentApp(process.env.TURBO_ENV)
+	})
+	.then(site => {
+		data['site'] = site
+		data['global'] = site.globalConfig
+		res.render('post', data)
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+module.exports = router
